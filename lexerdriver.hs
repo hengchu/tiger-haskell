81d05739d429c7a7d9aa4717aa390fa7dcddf3de
hengchu <hengchu.zhang@gmail.com>
2015-01-21 22:53:15 -0500

    implemented new tokenizer with Alex, it's much more better than the handwritten one...

--- lexerdriver.hs
+++ lexerdriver.hs
@@ -1,11 +1,11 @@
-import Lexer
+import TigerLexer
 import System.Environment
 
 main :: IO ()
 main = do args <- getArgs
           let file = args !! 0
           fileContent <- readFile file
-          let res = tokenize file fileContent
+          let res = scanner fileContent
           case res of
             Left perror -> print perror
             Right tokens -> print tokens
