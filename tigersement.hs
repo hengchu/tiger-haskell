bf65671b95ab52f18367279a31afd8bac347f4ac
hengchu <hengchu.zhang@gmail.com>
2015-01-28 17:56:40 -0500

    modified signature of tr functions

--- tigersement.hs
+++ tigersement.hs
@@ -10,9 +10,10 @@ import qualified TigerAbsyn as TAbsyn
 import qualified TigerSymbol as TSym
 import Data.Map
 import Control.Monad.State
+import Data.Either
 
 type SementError = (TLex.AlexPosn, String)
-type ExpTy = Either SementError TigSTy.Ty
+type ExpTy = TigSTy.Ty
 
 data EnvEntry = VarEntry {varTy::TigSTy.Ty, varReadOnly::Bool}
               | FunEntry {funLevel::Int, funFormals::[TigSTy.Ty], funResult::TigSTy.Ty}
@@ -31,24 +32,28 @@ genUnique = do (venv, tenv, a) <- get
                put (venv, tenv, a+1)
                return a
 
-transVar :: TAbsyn.Var -> SementState ExpTy
+transVar :: TAbsyn.Var -> SementState (Either SementError ExpTy)
 transVar = undefined
 
-transExp :: TAbsyn.Exp -> SementState ExpTy
+transExp :: TAbsyn.Exp -> SementState (Either SementError ExpTy)
 transExp = undefined
 
 transDec :: TAbsyn.Dec -> SementState (Either SementError ())
 transDec = undefined
 
-transTy :: TAbsyn.Ty -> SementState TigSTy.Ty
+transTy :: TAbsyn.Ty -> SementState (Either SementError TigSTy.Ty)
 transTy = undefined
 
 transProg' :: SementStates -> TAbsyn.Program -> Either SementError ()
 transProg' initialS (TAbsyn.Pexp e) = do _ <- evalState (transExp e) initialS
                                          return ()
 
-transProg' initialS (TAbsyn.Pdecs (d:decs)) = do let s = execState (transDec d) initialS
-                                                 transProg' s (TAbsyn.Pdecs decs)
+transProg' initialS (TAbsyn.Pdecs (d:decs)) = let (a, s) = runState (transDec d) initialS
+                                              in
+                                                case (isRight a) of 
+                                                  True  -> transProg' s (TAbsyn.Pdecs decs)
+                                                  False -> a
 transProg' _ (TAbsyn.Pdecs []) = return ()
 
+transProg :: TAbsyn.Program -> Either SementError ()
 transProg = transProg' emptyState
