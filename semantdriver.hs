afb1c04eaa502e38113e71f061dd87853673b530
hengchu <hengchu.zhang@gmail.com>
2015-03-01 19:39:56 -0500

    need to start 2nd round of code cleanup

--- semantdriver.hs
+++ semantdriver.hs
@@ -7,6 +7,7 @@ import Control.Monad.State
 import Control.Monad.Except
 import System.Environment
 import qualified TigerCanon as TCan
+import qualified TigerCodeGen as TCG
 import Data.List
 
 isdata (Frt.DATA _ _) = True
@@ -14,17 +15,24 @@ isdata (Frt.PROC _ _ _) = False
 isproc = not . isdata
 
 trans = do frags <- TS.transprog
+           (_, _, t, _, _) <- getState
            let datafrags = filter isdata frags
            let procfrags = filter isproc frags
            linearized <- mapM TCan.linearize $ map Frt.procBody procfrags
            basicblocks <- mapM TCan.basicblocks linearized
            traces <- mapM TCan.tracesched basicblocks
            let joined = concat traces
+           let foldhelper s (tmpcnt, instrs) = let (t2, instrs') = (TCG.codegen tmpcnt s)
+                                               in  (t2, instrs++instrs')
+           let results = foldr foldhelper (t, []) joined
            let datastrs = map Frt.prettyprintfrag datafrags
-           let codestrs = map Frt.prettyprintstm joined
            let datastr = (concat . intersperse "\n") datastrs
+           return $ datastr:(map show $ snd results)
+           {-
+           let codestrs = map Frt.prettyprintstm joined
            let codestr = (concat . intersperse "\n") codestrs
            return $ codestr ++ "\n" ++ datastr
+           -}
            
 
 main :: IO ()
@@ -41,4 +49,4 @@ main = do args <- getArgs
                             case stuff of
                               Left parserr -> print parserr
                               Right (Left semanterr) -> print semanterr
-                              Right (Right output) -> putStrLn output
+                              Right (Right output) -> mapM_ putStrLn output
--- tigerregisters.hs
+++ tigerregisters.hs
@@ -1,8 +1,17 @@
 module TigerRegisters
   (
     Register(..)
+  , npseudoregs
+  , availregs
   )
   where
 
-data Register = EAX | EBX | ECX | EDX | EBP | ESP | ESI | EDI
+data Register = EAX | EBX | ECX | EDX | EBP | ESP | ESI | EDI | ZERO
+                    | PSEUDO Int
                 deriving (Eq, Show)
+
+npseudoregs :: Int
+npseudoregs = 20
+
+availregs :: [Register]
+availregs = [EAX, EBX, ECX, EDX, EBP, ESP, ESI, EDI] ++ (map PSEUDO [1..npseudoregs])
